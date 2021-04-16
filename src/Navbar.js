3735e70c2b6c052b96c26c0512fab56aee354e3b
heldercepeda <helder_cepeda@hotmail.com>
2021-04-16 17:15:36 +0100

    Navbar in progress

---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (
+        <div className="p-4">
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="/about">About us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="/contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
